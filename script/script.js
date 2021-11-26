645c74c63f0c7300f716e766ca5b2a701bb6eda0
RafaelMALima <81188798+RafaelMALima@users.noreply.github.com>
2021-11-26 10:22:38 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(event){
     botao = document.querySelector('#back')
     botao.addEventListener('click', function(){
-        window.history.back()
+        window.history.back(-1)
         console.log(document.history.back());
 })
 })
