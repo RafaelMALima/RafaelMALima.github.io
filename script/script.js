27e29262f093e880bbc1720e7063fdd54d2f428d
RafaelMALima <81188798+RafaelMALima@users.noreply.github.com>
2021-12-07 11:51:10 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(event){
+    console.log(document.referrer)
+    console.log(document.location.host)
+    if (document.referrer == document.location.host){
+        texto = document.createElement('p')
+        texto.innerHTML = "A pagina que voce esta tentando acessar ainda nao esta pronta!"
+        texto.style.color = "#FF0000"
+        texto.style.align = 'center'
+        document.querySelector('body').appendChild(texto)
+    }
     botao = document.querySelector('#back')
     botao.addEventListener('click', function(){
         window.history.back(-1)
