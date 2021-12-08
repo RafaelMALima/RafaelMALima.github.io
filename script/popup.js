d037e44f71357bf33479acc299d6604891669997
gtumang <gabrielt3@al.insper.edu.br>
2021-12-08 00:32:27 -0300

    Alteracoes fabulosas

---
+++ script/popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(event){
+
+    button = document.querySelector("#sim")
+    button.addEventListener("click", function(){
+        document.querySelector('section').style.display = "none";
+        console.log('asdaqdsadsas')
+    })
+})
